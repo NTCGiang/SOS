52fb0b5f4cd1b837385b8850f1304d801cb91a3d
NTCGiang <giangntc20411@st.uel.edu.vn>
2022-11-30 17:14:21 +0700

    HI

--- New folder/app/src/main/java/com/test/login/IdentifyActivity.java
+++ New folder/app/src/main/java/com/test/login/IdentifyActivity.java
@@ -2,7 +2,9 @@ package com.test.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class IdentifyActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class IdentifyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identify);
+    }
+
+    public void txtDangNhap(View view) {
+        startActivity(new Intent(IdentifyActivity.this, RegisterActivity.class));
+    }
+
+    public void BackToWelcome(View view) {
+        startActivity(new Intent(IdentifyActivity.this, MainActivity.class));
     }
 }
--- New folder/app/src/main/java/com/test/login/RegisterActivity.java
+++ New folder/app/src/main/java/com/test/login/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.test.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+//    public void register(View view) {
+//        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+//    }
+
+    public void BackToWelcome(View view) {
+        startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+    }
+
+    public void txtDangNhap(View view) {
+        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
     }
 }
