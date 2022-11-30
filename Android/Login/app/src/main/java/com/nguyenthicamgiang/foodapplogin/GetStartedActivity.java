e4228686c0c7182d0f36f2a45db13dcc0e361837
NTCGiang <giangntc20411@st.uel.edu.vn>
2022-11-30 11:39:07 +0700

    HI

---
+++ Android/Login/app/src/main/java/com/nguyenthicamgiang/foodapplogin/GetStartedActivity.java
@@ -0,0 +1,25 @@
+package com.nguyenthicamgiang.foodapplogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class GetStartedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_started);
+
+        findViewById(R.id.get_started_txt).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(GetStartedActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        });
+    }
+}
---
+++ New folder/app/src/main/java/com/test/login/MainActivity.java
@@ -0,0 +1,28 @@
+package com.test.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        startActivity( new Intent(MainActivity.this,LoginActivity.class));
+    }
+
+    public void register(View view) {
+        startActivity (new Intent(MainActivity.this, RegisterActivity.class));
+    }
+}
